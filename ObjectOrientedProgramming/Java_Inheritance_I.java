e53c318fe01076c50e942f4a668f1ffccaae2a26
ashishkr16 <kumar.ashish.raj1997@gmail.com>
2021-04-08 02:51:58 +0530

    latest commit

---
+++ ObjectOrientedProgramming/Java_Inheritance_I.java
@@ -0,0 +1,31 @@
+package ObjectOrientedProgramming;
+/**
+ * InnerJava_Inheritance_I
+ */
+
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
+
+public class Java_Inheritance_I {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+	    bird.walk();
+	    bird.fly();
+        bird.sing();
+    }
+    
+}
